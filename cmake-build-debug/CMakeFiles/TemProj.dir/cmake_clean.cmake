9a2b42194c06b995611a44843773d5b363e3002b
faradayin <yinfeng.v@gmail.com>
2019-12-18 19:55:36 +0800

    remove useless folders

---
+++ cmake-build-debug/CMakeFiles/TemProj.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TemProj.dir/main.cpp.o"
+  "TemProj.pdb"
+  "TemProj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TemProj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
