149ae7516a098748be3a5db100567d22042df767
faradayin <yinfeng.v@gmail.com>
2019-12-19 17:16:10 +0800

    lx12.20

--- StrBlob.h
+++ StrBlob.h
@@ -16,11 +16,42 @@
 
 using std::vector; using std::string;
 
+class StrBlobPtr;
+
+class StrBlob
+{
+public:
+    typedef vector<string>::size_type size_type;
+
+    friend class StrBlobPtr;
+
+    StrBlobPtr begin();
+    StrBlobPtr end();
+
+    StrBlob(): data(std::make_shared<vector<string>>()) {};
+    StrBlob(std::initializer_list<string> il): data(std::make_shared<vector<string>>(il)) {}
+
+    size_type size() const {return data->size();}
+    bool empty() const {return data->empty();}
+    void push_back(const string &t) {data->push_back(t);};
+    void pop_back();
+    string& front();
+    string& back();
+
+private:
+    void check(size_type i, const std::string &) const;//如果索引i超出范围，抛出异常
+    std::shared_ptr<std::vector<std::string>> data;
+
+};
+
 class StrBlobPtr
 {
 public:
     StrBlobPtr(): curr(0) {}
     StrBlobPtr(StrBlob &a, size_t sz=0): wptr(a.data), curr(sz) {}
+
+    //重载算符!=
+    bool operator!=(const StrBlobPtr& p) {return p.curr != curr;}
 
     string& deref() const
     {
@@ -35,6 +66,7 @@ public:
         return *this;
     }
 
+
 private:
     //如果指针管理的对象存在，且索引i正确，返回管理对象的shared_ptr智能指针
     std::shared_ptr<vector<string>> check(size_t i, const string &msg) const
@@ -48,68 +80,5 @@ private:
     std::weak_ptr<vector<string>> wptr;
     size_t curr;
 };
-
-class StrBlob
-{
-public:
-    typedef vector<string>::size_type size_type;
-
-    friend class StrBlobPtr;
-
-    StrBlobPtr begin();
-    StrBlobPtr end();
-
-    StrBlob();
-    StrBlob(std::initializer_list<string> il);
-
-    size_type size() const {return data->size();}
-    bool empty() const {return data->empty();}
-    void push_back(const string &t) {data->push_back(t);};
-    void pop_back();
-    string& front();
-    string& back();
-
-private:
-    void check(size_type i, const std::string &msg) const;//如果索引i超出范围，抛出异常
-    std::shared_ptr<std::vector<std::string>> data;
-
-};
-StrBlob::StrBlob(): data(std::make_shared<vector<string>>()) {}
-StrBlob::StrBlob(std::initializer_list<string> il): data(std::make_shared<vector<string>>(il)) {}
-
-void StrBlob::check(size_type i, const string &msg) const
-{
-    if(i >= data->size())
-        throw std::out_of_range(msg);
-}
-
-string& StrBlob::front()
-{
-    check(0, "front on empty StrBlob");
-    return data->front();
-}
-
-string& StrBlob::back()
-{
-    check(0, "back on empty StrBlob");
-    return data->back();
-}
-
-void StrBlob::pop_back()
-{
-    check(0, "pop_back on empty StrBlob");
-    data->pop_back();
-}
-
-StrBlobPtr StrBlob::begin()
-{
-    return StrBlobPtr(*this);
-}
-StrBlobPtr StrBlob::end()
-{
-    return StrBlobPtr(*this, data->size());
-}
-
-
 
 #endif //TEMPROJ_STRBLOB_H
--- main.cpp
+++ main.cpp
@@ -3,9 +3,13 @@
 #include "StrBlob.h"
 int main()
 {
-    std::ifstream ifs("jieqi.txt");
+    std::ifstream ifs("../jieqi.txt");
     StrBlob j_blob;
-    for(std::string str; std::getline(ifs, str); j_blob.push_back(str));
+    for(std::string str; std::getline(ifs, str); j_blob.push_back(str))
+    {
+        std::cout<<str<<std::endl;
+    }
+    std::cout<<"reading via StrBlotPtr:\n";
     for(StrBlobPtr pbeg(j_blob.begin()), pend(j_blob.end()); pbeg != pend; pbeg.incr())
     {
         std::cout<<pbeg.deref()<< std::endl;
