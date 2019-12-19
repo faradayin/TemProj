2eee481c66e8b37afe55a1573340e423b0fd5dfd
faradayin <yinfeng.v@gmail.com>
2019-12-19 18:58:03 +0800

    from home

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-#include <fstream>
-#include "StrBlob.h"
+//#include <fstream>
+//#include "StrBlob.h"
 int main()
 {
-    std::ifstream ifs("jieqi.txt");
-    StrBlob j_blob;
-    for(std::string str; std::getline(ifs, str); j_blob.push_back(str));
-    for(StrBlobPtr pbeg(j_blob.begin()), pend(j_blob.end()); pbeg != pend; pbeg.incr())
-    {
-        std::cout<<pbeg.deref()<< std::endl;
-    }
-
+//    std::ifstream ifs("jieqi.txt");
+//    StrBlob j_blob;
+//    for(std::string str; std::getline(ifs, str); j_blob.push_back(str));
+//    for(StrBlobPtr pbeg(j_blob.begin()), pend(j_blob.end()); pbeg != pend; pbeg.incr())
+//    {
+//        std::cout<<pbeg.deref()<< std::endl;
+//    }
+    std::cout<<"小向美"<<std::endl;
     return 0;
 }
